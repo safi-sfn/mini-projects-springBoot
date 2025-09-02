b5d104bafb461f72dbaa9a7e86dcb12c2f0ba924
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-02 15:21:19 +0530

    feat(main) : main application to run program

---
+++ ProductStore/src/main/java/in/inxod/productStore/ProductStoreApplication.java
@@ -0,0 +1,13 @@
+package in.inxod.productStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStoreApplication.class, args);
+	}
+
+}
