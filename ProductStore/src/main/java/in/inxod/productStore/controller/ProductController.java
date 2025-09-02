fe96bb755778f114998963028695df2acc059ea8
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-02 15:26:17 +0530

    feat(controller) : all endpoint comes here

---
+++ ProductStore/src/main/java/in/inxod/productStore/controller/ProductController.java
@@ -0,0 +1,59 @@
+package in.inxod.productStore.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import in.inxod.productStore.model.Product;
+import in.inxod.productStore.service.ProductServiceImpl;
+
+@Controller
+@RequestMapping("/ps")
+public class ProductController {
+
+	@Autowired
+	private ProductServiceImpl service;
+
+	
+	@GetMapping("/show-product")
+	public String showAllProduct(Model model) {
+		List<Product> allProducts = service.getAllProduct();
+		model.addAttribute("products", allProducts);
+		
+		List<String> categories = service.getAllCategories();
+	    model.addAttribute("categories", categories);
+		return "dashboard";
+	}
+	
+
+	@GetMapping("/category")
+	public String showByCategory(@RequestParam("category") String category, Model model) {
+	    List<Product> products = service.getProductsByCategory(category);
+	    model.addAttribute("products", products);
+	
+	    List<String> categories = service.getAllCategories();
+	    model.addAttribute("categories", categories);
+	
+	    return "dashboard";
+	}
+	
+	@GetMapping("/new-product")
+	public String showForm(Model model) {
+		model.addAttribute("customer", new Product());
+		return "addProduct";
+	}
+	
+	@PostMapping("/add-product")
+	public String addProduct(Model model ,Product product) {
+		Product p = service.addProduct(product);
+		model.addAttribute("newProduct", p);
+		return "redirect:/ps/show-product";
+	}
+
+}
