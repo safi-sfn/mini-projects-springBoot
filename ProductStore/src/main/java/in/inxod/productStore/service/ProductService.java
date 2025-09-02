8838bfd7e330bddd3070c06ac882e47ada205022
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-02 21:26:10 +0530

    feat(service) : defining more method for CRUD

--- ProductStore/src/main/java/in/inxod/productStore/service/ProductService.java
+++ ProductStore/src/main/java/in/inxod/productStore/service/ProductService.java
@@ -11,10 +11,17 @@ public interface ProductService {
 	List<Product> getAllProduct();
 	
 	List<Product> getProductsByCategory(String category);
-	
 	List<String> getAllCategories();
 	
+	List<Product> getProductByBrand(String brand);
+	List<String> getAllBrand();
+	
 	Product addProduct(Product product);
+	
+	Product updateProduct(Product product);
+	Product getProductById(Integer id);
+	
+	void deleteProductRecord(Integer id);
 
 	
 	
