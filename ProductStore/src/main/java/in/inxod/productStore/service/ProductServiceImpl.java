474430fe0ef341356af2080e840548540ad78454
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-02 15:25:05 +0530

    feat(service): implementation class for service interface

---
+++ ProductStore/src/main/java/in/inxod/productStore/service/ProductServiceImpl.java
@@ -0,0 +1,67 @@
+package in.inxod.productStore.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import in.inxod.productStore.model.Product;
+import in.inxod.productStore.repository.ProductRepository;
+
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	
+	@Autowired
+	private ProductRepository repo;
+	
+
+//============================================	
+	
+	@Override
+	public List<Product> getAllProduct() {
+		
+		return repo.findAll();
+	}
+	
+	@Override
+	public List<Product> getProductsByCategory(String category) {
+	    return repo.findByBrandCategory(category);
+	}
+	
+	
+	@Override
+	public List<String> getAllCategories() {
+	    return repo.findDistinctCategories();
+	}
+
+	@Override
+	public Product addProduct(Product product) {
+		
+		return repo.save(product);
+	}
+
+
+	
+	
+	
+
+	
+}
+
+
+
+
+
+
+
+
+
+
