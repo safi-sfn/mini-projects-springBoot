8c36426432971438e897a8f9267e652bb5c8d1c7
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-03 12:23:12 +0530

    initial commit

---
+++ HealthCureLink/src/test/java/in/inxod/hcl/HealthCureLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.hcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCureLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
