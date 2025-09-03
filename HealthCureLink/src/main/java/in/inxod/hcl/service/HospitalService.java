03d9368683f77ca3f4910c2a4cabdd27675da3f8
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-03 12:25:45 +0530

    feat(service) : skeleton of HCL project

---
+++ HealthCureLink/src/main/java/in/inxod/hcl/service/HospitalService.java
@@ -0,0 +1,5 @@
+package in.inxod.hcl.service;
+
+public interface HospitalService {
+
+}
---
+++ HealthCureLink/src/main/java/in/inxod/hcl/service/HospitalServiceImpl.java
@@ -0,0 +1,8 @@
+package in.inxod.hcl.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HospitalServiceImpl implements HospitalService {
+
+}
