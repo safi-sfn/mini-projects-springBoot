879550dec472cfead78754dbfc9da129f8635d7f
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-03 12:24:55 +0530

    feat(main) :

---
+++ HealthCureLink/src/main/java/in/inxod/hcl/HealthCureLinkApplication.java
@@ -0,0 +1,13 @@
+package in.inxod.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCureLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCureLinkApplication.class, args);
+	}
+
+}
