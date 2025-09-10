4787d2bd96788057f6da80a53ea5b4adc052dad6
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-10 16:19:25 +0530

    feat(service) : defining some method

--- HealthCureLink/src/main/java/in/inxod/hcl/service/HospitalService.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/service/HospitalService.java
@@ -1,5 +1,17 @@
 package in.inxod.hcl.service;
 
-public interface HospitalService {
+import java.util.List;
 
+import in.inxod.hcl.entity.Doctor;
+import in.inxod.hcl.entity.Patient;
+
+public interface HospitalService {
+	
+	  // Doctor related
+    Doctor addDoctor(Doctor doctor);
+    public List<Doctor>getAllDoctor();
+
+    // Patient related
+    Patient addPatient(Patient patient);
+    public List<Patient>getAllPatient();
 }
