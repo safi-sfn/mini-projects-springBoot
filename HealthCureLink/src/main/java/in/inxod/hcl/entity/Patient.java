47c9ab502a00eda74846096913376aad589df8bd
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-10 16:17:43 +0530

    feat(entity) : patient Entity class

--- HealthCureLink/src/main/java/in/inxod/hcl/entity/Patient.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/entity/Patient.java
@@ -3,6 +3,8 @@ package in.inxod.hcl.entity;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,8 +13,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="patient")
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,9 +28,84 @@ public class Patient {
     private LocalDate admittedDate;
 
     @ManyToOne
-    @JoinColumn(name = "doctor_id")
+    @JoinColumn(name = "doctor_id", referencedColumnName = "doctorId")
+    @JsonIgnoreProperties("patients")  // doctor ke andar patients ignore karega
     private Doctor doctor;
 
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
     private List<Appointment> appointments;
+
+	public Integer getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(Integer patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getDisease() {
+		return disease;
+	}
+
+	public void setDisease(String disease) {
+		this.disease = disease;
+	}
+
+	public LocalDate getAdmittedDate() {
+		return admittedDate;
+	}
+
+	public void setAdmittedDate(LocalDate admittedDate) {
+		this.admittedDate = admittedDate;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+
+	public List<Appointment> getAppointments() {
+		return appointments;
+	}
+
+	public void setAppointments(List<Appointment> appointments) {
+		this.appointments = appointments;
+	}
+
+	public Patient(String name, Integer age, String disease, LocalDate admittedDate, Doctor doctor,
+			List<Appointment> appointments) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.disease = disease;
+		this.admittedDate = admittedDate;
+		this.doctor = doctor;
+		this.appointments = appointments;
+	}
+
+	public Patient() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+    
 }
