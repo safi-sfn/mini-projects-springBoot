af3457e404b4c3c2c85349573231f4331c755d18
safi-sfn <mdsafiruddin24@gmail.com>
2025-09-10 16:18:51 +0530

    feat(appointment) : Entity for appointment

--- HealthCureLink/src/main/java/in/inxod/hcl/entity/Appointment.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/entity/Appointment.java
@@ -8,8 +8,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="appointment")
 public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +27,59 @@ public class Appointment {
     @ManyToOne
     @JoinColumn(name = "doctor_id")
     private Doctor doctor;
+
+	public Integer getAppointmentId() {
+		return appointmentId;
+	}
+
+	public void setAppointmentId(Integer appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+
+	public LocalDate getAppointmentDate() {
+		return appointmentDate;
+	}
+
+	public void setAppointmentDate(LocalDate appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+
+	public Appointment(LocalDate appointmentDate, String reason, Patient patient, Doctor doctor) {
+		super();
+		this.appointmentDate = appointmentDate;
+		this.reason = reason;
+		this.patient = patient;
+		this.doctor = doctor;
+	}
+
+	public Appointment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+    
 }
