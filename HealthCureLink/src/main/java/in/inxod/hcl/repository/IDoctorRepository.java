1115bd2566737250a45437ccd1f37efc2259206b
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-06 19:53:07 +0530

    feat(repo) : Doctor repository. added custom finder method

--- HealthCureLink/src/main/java/in/inxod/hcl/repository/IDoctorRepository.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/repository/IDoctorRepository.java
@@ -1,5 +1,7 @@
 package in.inxod.hcl.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,10 @@ import in.inxod.hcl.entity.Doctor;
 @Repository
 public interface IDoctorRepository extends JpaRepository<Doctor, Integer> {
 
+	List<Doctor> findBySpecialization(String specialization);
+
+	List<Doctor> findByExperienceGreaterThan(Integer experience);
+	
+	
+	
 }
