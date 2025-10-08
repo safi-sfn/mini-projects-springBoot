2ddc6ce116d927bb1ef7e9a4b1205ccae07d8b44
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-08 14:02:06 +0530

    refactor

--- HealthCureLink/src/main/java/in/inxod/hcl/service/HospitalService.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/service/HospitalService.java
@@ -9,23 +9,24 @@ public interface HospitalService {
 	
 	  //======== Doctor related ==========
 	
-	//  Add a new doctor
+	 //  Add a new doctor
     public Doctor addDoctor(Doctor doctor);
-    //  Get all doctors
+     //  Get all doctors
     public List<Doctor>getAllDoctor();
-    // Get Doctor By Id
+     // Get Doctor By Id
     public Doctor getDoctorById(Integer doctorId);
-    //  Update Doctor Details
+     //  Update Doctor Details
     public Doctor updateDoctor(Doctor doctor);
     //  Delete Doctor
-    void deleteDoctor(Integer doctorId);  
+    public String deleteDoctor(Integer doctorId);  
     // Get Doctor By specialization
     public List<Doctor> getDoctorBySpecialization(String specialization);
     // Get Doctor with experience greater than X year
     public List<Doctor> getDoctorByExperienceGreaterThan(Integer experience);
    
 
-    // Patient related
+      //======== Patient related ==========
+    
     public Patient addPatient(Patient patient);
     public List<Patient>getAllPatient();
 }
