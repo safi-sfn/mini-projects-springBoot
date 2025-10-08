71814d0995334b6600f42b80049d349f4d5ea75e
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-08 18:20:44 +0530

    feat(controller) : Doctor By Specialization EndPoint created

--- HealthCureLink/src/main/java/in/inxod/hcl/controller/DoctorController.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/controller/DoctorController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import in.inxod.hcl.entity.Doctor;
 import in.inxod.hcl.service.HospitalServiceImpl;
 
@@ -25,74 +24,90 @@ public class DoctorController {
 
 	@Autowired
 	private HospitalServiceImpl doctorService;
-	
-	
-	//============DOCTOR-RELATED=================
-	
+
+	// ============DOCTOR-RELATED=================
+
 	@PostMapping("/add")
 	public ResponseEntity<Doctor> addDoctor(@RequestBody Doctor doctor) {
-		Doctor newDoctor =  doctorService.addDoctor(doctor);
+		Doctor newDoctor = doctorService.addDoctor(doctor);
 		return new ResponseEntity<Doctor>(newDoctor, HttpStatus.CREATED);
 	}
+
 	@GetMapping("/getAll")
-	public ResponseEntity<List<Doctor>> getAllDoctor(){
+	public ResponseEntity<List<Doctor>> getAllDoctor() {
 		List<Doctor> allDoctors = doctorService.getAllDoctor();
 		return new ResponseEntity<List<Doctor>>(allDoctors, HttpStatus.OK);
 	}
-	
+
 	@PutMapping("/update/{id}")
-	public ResponseEntity<Doctor> updateDoctor(@PathVariable Integer id, @RequestBody Doctor doctor){
+	public ResponseEntity<Doctor> updateDoctor(@PathVariable Integer id, @RequestBody Doctor doctor) {
 		Doctor existingDoctor = doctorService.getDoctorById(id);
-		if(existingDoctor == null) {
+		if (existingDoctor == null) {
 			return ResponseEntity.notFound().build();
 		}
-		
+
 		existingDoctor.setName(doctor.getName());
 		existingDoctor.setExperience(doctor.getExperience());
 		existingDoctor.setSalary(doctor.getSalary());
 		existingDoctor.setSpecialization(doctor.getSpecialization());
-		
-		Doctor updateDoctor= doctorService.addDoctor(existingDoctor);
-		return new ResponseEntity<Doctor>(updateDoctor,HttpStatus.OK);
-		
+
+		Doctor updateDoctor = doctorService.addDoctor(existingDoctor);
+		return new ResponseEntity<Doctor>(updateDoctor, HttpStatus.OK);
+
 	}
-	
+
 	@PatchMapping("update-field/{id}")
-	public ResponseEntity<Doctor> updateDoctorFields(@PathVariable Integer id, @RequestBody Doctor doctor){
+	public ResponseEntity<Doctor> updateDoctorFields(@PathVariable Integer id, @RequestBody Doctor doctor) {
 		Doctor existing = doctorService.getDoctorById(id);
-	    if (existing == null) {
-	        return ResponseEntity.notFound().build();
-	    }
+		if (existing == null) {
+			return ResponseEntity.notFound().build();
+		}
 
-	    // Update only provided fields
-	    if (doctor.getName() != null) {
-	    	existing.setName(doctor.getName());
-	    }
-	    if (doctor.getSpecialization() != null) {
-	    	existing.setSpecialization(doctor.getSpecialization());
-	    }
-	    if (doctor.getExperience() != null) {
-	    	existing.setExperience(doctor.getExperience());
-	    }
-	    if (doctor.getSalary() != null) {
-	    	existing.setSalary(doctor.getSalary());
-	    }
-	    
-	   Doctor updated =  doctorService.addDoctor(existing);
+		// Update only provided fields
+		if (doctor.getName() != null) {
+			existing.setName(doctor.getName());
+		}
+		if (doctor.getSpecialization() != null) {
+			existing.setSpecialization(doctor.getSpecialization());
+		}
+		if (doctor.getExperience() != null) {
+			existing.setExperience(doctor.getExperience());
+		}
+		if (doctor.getSalary() != null) {
+			existing.setSalary(doctor.getSalary());
+		}
 
-	    return new ResponseEntity<Doctor>(updated,HttpStatus.OK);
+		Doctor updated = doctorService.addDoctor(existing);
+
+		return new ResponseEntity<Doctor>(updated, HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<String> deleteDoctor(@PathVariable Integer id){
+	public ResponseEntity<String> deleteDoctor(@PathVariable("id") Integer id) {
 		String status = doctorService.deleteDoctor(id);
-		return new ResponseEntity<String>(status,HttpStatus.OK);
+		return new ResponseEntity<String>(status, HttpStatus.OK);
 	}
 	
 	
-	
+	@GetMapping("/specialization/{drSpslize}")
+	public ResponseEntity<?> getDoctorBySpecialization(@PathVariable("drSpslize") String drSpslize){
+		List<Doctor> drList = doctorService.getDoctorBySpecialization(drSpslize);
+		if(drList.isEmpty()) {
+			String msg = "No doctors found with specialization: " + drSpslize;
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body(msg);
+		}
+		return new ResponseEntity<List<Doctor>>(drList,HttpStatus.OK);
+	}
 
 }
+
+
+
+
+
+
 
 
 
