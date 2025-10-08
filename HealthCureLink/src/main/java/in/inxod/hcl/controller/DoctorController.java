a2d2fe8c626518da932a102a354539d24ca48213
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-08 13:38:03 +0530

    feat(controller) : Patch updating endpoint created

--- HealthCureLink/src/main/java/in/inxod/hcl/controller/DoctorController.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/controller/DoctorController.java
@@ -6,14 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import in.inxod.hcl.entity.Doctor;
-import in.inxod.hcl.entity.Patient;
 import in.inxod.hcl.service.HospitalServiceImpl;
 
 @RestController
@@ -37,9 +39,9 @@ public class DoctorController {
 		return new ResponseEntity<List<Doctor>>(allDoctors, HttpStatus.OK);
 	}
 	
-	@PutMapping("/update")
-	public ResponseEntity<Doctor> updateDoctor(@RequestBody Doctor doctor){
-		Doctor existingDoctor = doctorService.getDoctorById(doctor.getDoctorId());
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Doctor> updateDoctor(@PathVariable Integer id, @RequestBody Doctor doctor){
+		Doctor existingDoctor = doctorService.getDoctorById(id);
 		if(existingDoctor == null) {
 			return ResponseEntity.notFound().build();
 		}
@@ -52,6 +54,32 @@ public class DoctorController {
 		Doctor updateDoctor= doctorService.addDoctor(existingDoctor);
 		return new ResponseEntity<Doctor>(updateDoctor,HttpStatus.OK);
 		
+	}
+	
+	@PatchMapping("update-field/{id}")
+	public ResponseEntity<Doctor> updateDoctorFields(@PathVariable Integer id, @RequestBody Doctor doctor){
+		Doctor existing = doctorService.getDoctorById(id);
+	    if (existing == null) {
+	        return ResponseEntity.notFound().build();
+	    }
+
+	    // Update only provided fields
+	    if (doctor.getName() != null) {
+	    	existing.setName(doctor.getName());
+	    }
+	    if (doctor.getSpecialization() != null) {
+	    	existing.setSpecialization(doctor.getSpecialization());
+	    }
+	    if (doctor.getExperience() != null) {
+	    	existing.setExperience(doctor.getExperience());
+	    }
+	    if (doctor.getSalary() != null) {
+	    	existing.setSalary(doctor.getSalary());
+	    }
+	    
+	   Doctor updated =  doctorService.addDoctor(existing);
+
+	    return new ResponseEntity<Doctor>(updated,HttpStatus.OK);
 	}
 	
 	
