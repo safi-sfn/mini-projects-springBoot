58957c81a659d51a54a4e027b56de95dfe374bf8
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-10 22:18:17 +0530

    feat(patientCotroller) :update controller API created

--- HealthCureLink/src/main/java/in/inxod/hcl/controller/PatientController.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/controller/PatientController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import in.inxod.hcl.entity.Doctor;
 import in.inxod.hcl.entity.Patient;
 import in.inxod.hcl.service.HospitalService;
 
@@ -35,4 +37,41 @@ public class PatientController {
 			return new ResponseEntity<List<Patient>>(patients,HttpStatus.OK);
 		}
 		
+		@PatchMapping("/update/{id}")
+		public ResponseEntity<Patient> updatePatient(@RequestBody Patient patient, @PathVariable Integer id, Integer drId){
+
+			Patient existingPatient = patientService.getPatientById(id);
+			if(existingPatient==null) {
+				return ResponseEntity.notFound().build();
+			}
+			
+			// Update only provided fields
+			if (patient.getName() != null) {
+				existingPatient.setName(patient.getName());
+			}
+			if (patient.getDoctor() != null) {
+				existingPatient.setDoctor(patient.getDoctor());
+			}
+			if (patient.getDisease() != null) {
+				existingPatient.setDisease(patient.getDisease());
+			}
+			if (patient.getAge()!= null) {
+				existingPatient.setAge(patient.getAge());
+			}
+
+			Patient updated = patientService.addPatient(existingPatient,drId);
+
+			return new ResponseEntity<Patient>(updated, HttpStatus.OK);
+		}
+		
 }
+
+
+
+
+
+
+
+
+
+
