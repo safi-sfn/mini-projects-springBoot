19de051031c57db572af0a24ab0da67634c3d1e6
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-14 10:53:00 +0530

    refactor

--- HealthCureLink/src/main/java/in/inxod/hcl/controller/AppointmentController.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/controller/AppointmentController.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.inxod.hcl.service.HospitalService;
+import in.inxod.hcl.service.HospitalServiceImpl;
 
 @RestController
 public class AppointmentController {
 
 	@Autowired
-	private HospitalService appointmtService;
+	private HospitalServiceImpl appointmtService;
 	
 	//============APPOINTMENT-RELATED============
 }
