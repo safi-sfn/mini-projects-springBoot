7bbe921701e145205a398130f4af34b3ba718b5f
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-16 20:37:06 +0530

    refactor

--- HealthCureLink/src/main/java/in/inxod/hcl/controller/AppointmentController.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/controller/AppointmentController.java
@@ -3,7 +3,6 @@ package in.inxod.hcl.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-import in.inxod.hcl.service.HospitalService;
 import in.inxod.hcl.service.HospitalServiceImpl;
 
 @RestController
