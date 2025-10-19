d236ecda936c42896bc98731ef5e1f7303122e7f
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-19 18:54:43 +0530

    refactor

--- HealthCureLink/src/main/java/in/inxod/hcl/repository/IAppointmentRepository.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/repository/IAppointmentRepository.java
@@ -2,7 +2,6 @@ package in.inxod.hcl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import in.inxod.hcl.entity.Appointment;
 
 @Repository
