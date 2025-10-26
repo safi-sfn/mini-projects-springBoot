659181881b611bf6bb2aed95a6426013b18e6ca6
safi-sfn <mdsafiruddin24@gmail.com>
2025-10-26 22:21:23 +0530

    refactor

--- ProductStore/src/main/java/in/inxod/productStore/service/ProductServiceImpl.java
+++ ProductStore/src/main/java/in/inxod/productStore/service/ProductServiceImpl.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import in.inxod.productStore.model.Product;
