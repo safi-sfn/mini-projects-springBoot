571f4a220566e8f97cc10c81b20b8bbe9c8275f2
safi-sfn <mdsafiruddin24@gmail.com>
2025-11-13 19:06:14 +0530

    feat : book new appointment repo

--- HealthCureLink/src/main/java/in/inxod/hcl/repository/IAppointmentRepository.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/repository/IAppointmentRepository.java
@@ -7,4 +7,7 @@ import in.inxod.hcl.entity.Appointment;
 @Repository
 public interface IAppointmentRepository extends JpaRepository<Appointment, Integer>{
 
+	// Book New Appointment
+	public Appointment bookAppointment(Appointment appointment, Integer patientId, Integer doctorId);
+	
 }
