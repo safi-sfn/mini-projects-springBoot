8c3a731ba3becf4fa648b0b2766065a64af9d85c
safi-sfn <mdsafiruddin24@gmail.com>
2025-11-15 20:58:07 +0530

    refactor

--- HealthCureLink/src/main/java/in/inxod/hcl/repository/IAppointmentRepository.java
+++ HealthCureLink/src/main/java/in/inxod/hcl/repository/IAppointmentRepository.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Repository;
 import in.inxod.hcl.entity.Appointment;
 
 @Repository
-public interface IAppointmentRepository extends JpaRepository<Appointment, Integer>{
+public interface IAppointmentRepository extends JpaRepository<Appointment, Integer> {
 
 	// Book New Appointment
 	public Appointment bookAppointment(Appointment appointment, Integer patientId, Integer doctorId);
-	
-	 // Update appointment details
-    Appointment updateAppointment(Appointment appointment);
+
+	// Update appointment details
+	Appointment updateAppointment(Appointment appointment);
 }
