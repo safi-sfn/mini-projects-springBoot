20d7366a7c02127e1339cfb9b3a359239e58c7b7
safi-sfn <mdsafiruddin24@gmail.com>
2025-11-17 20:21:02 +0530

    initial commit

---
+++ patientAddmit/src/main/java/in/inxod/patientAddmit/PatientAddmitApplication.java
@@ -0,0 +1,13 @@
+package in.inxod.patientAddmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientAddmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientAddmitApplication.class, args);
+	}
+
+}
---
+++ patientAddmit/src/test/java/in/inxod/patientAddmit/PatientAddmitApplicationTests.java
@@ -0,0 +1,13 @@
+package in.inxod.patientAddmit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAddmitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
